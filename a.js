ae6bdc7edfbe5f12e9bc83c65d5b3f0dcfdb46be
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-07 21:20:38 +0700

    x

--- a.js
+++ a.js
@@ -1,2 +1,12 @@
-console.log(123);
-console.log('Tinh nang moi');
+console.log('Hello world.');
+
+// const name = 'Teo';
+// const age = 10;
+// console.log(name, age);
+const person = { name: 'Teo', age: 10 };
+person.name = 'Teo 2';
+console.log(person);
+
++1
+1 + 2
+'x' + 1
---
+++ b.js
@@ -0,0 +1,19 @@
+// try {
+//     console.log(x);
+//     console.log(2);
+// } catch (error) {
+//     console.log(error.stack)
+//     console.log(1);
+// }
+// try {
+//     throw new Error('Got an error.');
+// } catch (error) {
+//     console.log(error.message);
+// }
+let index = 0
+for (;;) {
+    if (index === 100) break;
+    console.log(index ++);
+}
+
+// https://github.com/vanpho93/docs/blob/master/BaiTap_JavaScript_NodeJS.pdf
